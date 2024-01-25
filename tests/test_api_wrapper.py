b0ca21acf297e3f0d35d8a1af2032dba2ecd6b09
Gordon Bailey <grodtron@users.noreply.github.com>
2024-01-25 09:44:21 +0100

    Update test_api_wrapper.py - WIP test

--- tests/test_api_wrapper.py
+++ tests/test_api_wrapper.py
@@ -1,4 +1,5 @@
 import unittest
+import uuid
 from unittest.mock import MagicMock
 from autodoist.models import ConcreteTodoistObjects, ConcreteTodoistLabel, ConcreteTodoistFilter, ConcreteTodoistProject
 from autodoist.todoist.api_wrapper import TodoistApiWrapper, TodoistAPIRequester
@@ -32,8 +33,8 @@ class TestTodoistApiWrapper(unittest.TestCase):
         # Set up the expected sync_commands and response
         todoist_objects = ConcreteTodoistObjects(
             labels=[ConcreteTodoistLabel(id=1, name="Label 1", color="#ffffff", is_favorite=True)],
-            filters=[ConcreteTodoistFilter(id=1, name="Filter 1", query="query", color="#ffffff", is_favorite=True)],
-            projects=[ConcreteTodoistProject(id=1, name="Project 1", color="#ffffff", is_favorite=True)]
+            filters=[], #[ConcreteTodoistFilter(id=1, name="Filter 1", query="query", color="#ffffff", is_favorite=True)],
+            projects=[] #[ConcreteTodoistProject(id=1, name="Project 1", color="#ffffff", is_favorite=True)]
         )
         expected_sync_commands = [
             {
@@ -47,14 +48,28 @@ class TestTodoistApiWrapper(unittest.TestCase):
 
         # Call the method to test
         self.todoist_api_wrapper.update_todoist_objects(todoist_objects)
-
+        
         # Assertions
         # Check that make_request was called with a subset of the expected commands
         self.mock_api_requester.make_request.assert_called_once()
         actual_args, actual_kwargs = self.mock_api_requester.make_request.call_args
         actual_commands = actual_kwargs['commands']
-        for expected_command in expected_sync_commands:
-            self.assertIn(expected_command, actual_commands)
+        
+        for actual_command in actual_commands:
+            # Ensure 'uuid' key exists and contains a well-formed UUID string
+            actual_uuid = actual_command.get('uuid')
+            self.assertIsNotNone(actual_uuid)
+            self.assertTrue(uuid.UUID(actual_uuid, version=4))
+        
+            # Delete 'uuid' key from actual_commands
+            del actual_command['uuid']
+
+            self.assertIn(actual_command, expected_commands)
+        
+        
+        # Check that actual_commands is now an empty dictionary
+        self.assertEqual(actual_commands, {})
+
 
 if __name__ == '__main__':
     unittest.main()
