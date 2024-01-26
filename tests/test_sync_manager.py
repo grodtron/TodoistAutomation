a53552165bc9f55d97a325e0a16ceb39bdea7ff8
Gordon Bailey <grodtron@users.noreply.github.com>
2024-01-26 02:04:45 +0100

    Update test_sync_manager.py

--- tests/test_sync_manager.py
+++ tests/test_sync_manager.py
@@ -1,6 +1,6 @@
 import unittest
 from parameterized import parameterized
-from autodoist.models import TodoistCollection, TodoistLabel, TodoistFilter, TodoistProject, ConcreteTodoistObjects, ConcreteTodoistLabel, ConcreteTodoistFilter, ConcreteTodoistProject
+from autodoist.models import TodoistCollection, TodoistLabel, TodoistFilter, TodoistProject, ConcreteTodoistObjects, ConcreteTodoistLabel, ConcreteTodoistFilter, ConcreteTodoistProject, ConcreteTodoistCollection
 from autodoist.todoist.api_wrapper import TodoistApiWrapper
 from autodoist.sync_manager import TodoistSyncManager
 
