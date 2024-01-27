6c20cc0801e578a1182b3cb6873117b63cf10000
Gordon Bailey <grodtron@users.noreply.github.com>
2024-01-27 14:48:33 +0100

    Update test_sync_manager.py

--- tests/test_sync_manager.py
+++ tests/test_sync_manager.py
@@ -59,17 +59,21 @@ class TestTodoistSyncManager(unittest.TestCase):
 
         # Test case 5: Add a New Filter
         ("AddNewFilter", TodoistCollection(
+            labels=[],
             filters=[
                 TodoistFilter(name="NewFilter", query="query2")
             ],
+            projects=[]
         ), ConcreteTodoistObjects(
             labels=[],
             filters=[],
             projects=[]
         ), ConcreteTodoistObjects(
+            labels=[],
             filters=[
                 ConcreteTodoistFilter(name="NewFilter", query="query2")
             ],
+            projects=[]
         )),
     ])
     def test_sync(self, name, desired_state, existing_state, expected_sync_commands):
