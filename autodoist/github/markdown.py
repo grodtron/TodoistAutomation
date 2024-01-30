1ed207ff4b827cb88fbbe4db71c53c928ccf74a7
Gordon Bailey <grodtron@users.noreply.github.com>
2024-01-31 00:58:34 +0100

    Create markdown.py with rendering function (#32)

---
+++ autodoist/github/markdown.py
@@ -0,0 +1,16 @@
+from autodoist.models import ConcreteTodoistObjects
+
+def render_as_markdown(todoist_objects: ConcreteTodoistObjects) -> str:
+    markdown = ""
+
+    for item in todoist_objects.get_all_items():
+        action = "Created" if item.id is None else "Updated"
+        markdown += f"### {item.get_type().capitalize()} ({action})\n"
+        markdown += "| Field | Value |\n"
+        markdown += "|-------|-------|\n"
+        for field_name, field_value in item.to_dict().items():
+            if field_value is not None:
+                markdown += f"| {field_name} | {field_value} |\n"
+        markdown += "\n"
+
+    return markdown
