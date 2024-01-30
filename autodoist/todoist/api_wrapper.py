69032963fbcefecd50e7732f281ca13334d7ee0e
Gordon Bailey <grodtron@users.noreply.github.com>
2024-01-30 01:11:46 +0100

    Add debug logging (#22)

--- autodoist/todoist/api_wrapper.py
+++ autodoist/todoist/api_wrapper.py
@@ -1,4 +1,6 @@
+
 from typing import Optional, List, Dict
+import logging
 import uuid
 import requests
 from autodoist.models import (
@@ -8,7 +10,6 @@ from autodoist.models import (
     ConcreteTodoistProject,
 )
 
-
 class TodoistAPIRequester:
     API_URL = "https://api.todoist.com/sync/v9/sync"
     HEADERS = {
@@ -17,21 +18,24 @@ class TodoistAPIRequester:
 
     def __init__(self, api_key: str):
         self.api_key = api_key
+        self.logger = logging.getLogger(__name__)
 
     def make_request(self, **payload) -> Dict:
+        self.logger.debug(f"Sending request to {self.API_URL} with payload: {payload}")
         response = requests.post(self.API_URL, headers=self.HEADERS, data=payload)
+        self.logger.debug(f"Received response: {response.content.decode('utf-8')}")
         return response.json()
-
 
 class TodoistApiWrapper:
     def __init__(self, api_requester: TodoistAPIRequester):
         self.api_requester = api_requester
+        self.logger = logging.getLogger(__name__)
 
     def get_all_todoist_objects(self) -> ConcreteTodoistObjects:
         response = self.api_requester.make_request(
             sync_token="*", resource_types=["labels", "filters", "projects"]
         )
-
+        self.logger.debug(f"Received Todoist objects: {response}")
         labels = [ConcreteTodoistLabel(**label) for label in response.get("labels", [])]
         filters = [
             ConcreteTodoistFilter(**filter_) for filter_ in response.get("filters", [])
@@ -49,7 +53,8 @@ class TodoistApiWrapper:
         for item in todoist_objects.get_all_items():
             item_type = item.get_type()
             sync_commands.append(self._create_update_command(item_type, item.id, item))
-
+        
+        self.logger.debug(f"Sending update commands: {sync_commands}")
         return self.api_requester.make_request(commands=sync_commands)
 
     def _create_update_command(
@@ -69,7 +74,6 @@ class TodoistApiWrapper:
 
         return command
 
-
 class DryRunTodoistApiWrapper(TodoistApiWrapper):
     def update_todoist_objects(self, todoist_objects: ConcreteTodoistObjects) -> Dict:
         sync_commands = []
@@ -78,8 +82,7 @@ class DryRunTodoistApiWrapper(TodoistApiWrapper):
             item_type = item.get_type()
             command = self._create_update_command(item_type, item.id, item)
             sync_commands.append(command)
-            print(f"Command to be executed: {command}")
+            self.logger.debug(f"Dry run command: {command}")
 
-        print("Dry run completed. No changes were made.")
-
+        self.logger.info("Dry run completed. No changes were made.")
         return dict()
